2cefbdab2934e4f2a5c4695c1b1d75406eea70ed
YU-FANG <jill83123@gmail.com>
2024-02-14 04:39:37 +0800

    feat: :sparkles: 發信功能介接

---
+++ README.md
@@ -0,0 +1,2 @@
+### 參考文章
+[Node.js 應用篇 - 使用 Nodemailer 來發送 Email](https://israynotarray.com/nodejs/20230722/1626712457/)
---
+++ routes/contact.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+
+const router = express.Router();
+require('dotenv').config();
+
+let reviewMessage = '';
+
+router.post('/post', async (req, res) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS,
+    },
+  });
+
+  // 驗證該服務是否可用
+  await transporter.verify();
+
+  const mailOptions = {
+    from: process.env.GMAIL_USER,
+    to: process.env.GMAIL_USER, // 發送的對象信箱
+    subject: `${req.body.username} 發送了一封信`, // 信件的主旨
+    text: req.body.description, // 信件的內容
+  };
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      reviewMessage = '發送失敗';
+      console.error(err);
+    } else {
+      reviewMessage = '發送成功';
+      console.log(info);
+    }
+    res.redirect('review');
+  });
+});
+
+router.get('/review', function (req, res) {
+  res.render('contactReview', { reviewMessage });
+});
+
+module.exports = router;
